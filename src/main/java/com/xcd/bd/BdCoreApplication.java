7d79fd9774df4529accb50472266d172a852cb27
lijinku <544195627@qq.com>
2019-06-21 11:48:56 +0800

    修改

--- src/main/java/com/xcd/bd/BdCoreApplication.java
+++ src/main/java/com/xcd/bd/BdCoreApplication.java
@@ -1,9 +1,11 @@
 package com.xcd.bd;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.xcd.bd.dao")
 public class BdCoreApplication {
 
 	public static void main(String[] args) {
