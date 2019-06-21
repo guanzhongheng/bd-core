e41e65d2dfa24ed3c9eb1f2676934a2dff9b9532
guanzhongheng <373284103@qq.com>
2019-06-21 11:48:56 +0800

    合并

---
+++ src/main/java/com/xcd/bd/controller/LoginController.java
@@ -0,0 +1,20 @@
+package com.xcd.bd.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class LoginController {
+
+
+    @RequestMapping(value = "login")
+    public String firstLogin(){
+        return "login";
+    }
+
+
+
+}
+
+
--- src/main/java/com/xcd/bd/service/impl/AcctInfoServiceImpl.java
+++ src/main/java/com/xcd/bd/service/impl/AcctInfoServiceImpl.java
@@ -14,6 +14,7 @@ import java.util.List;
 */
 @Service
 public class AcctInfoServiceImpl implements IAcctInfoService {
+
     @Autowired
     private TAcctInfoMapper tAcctInfoMapper;
 
